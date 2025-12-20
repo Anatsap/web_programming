ad766f344fd45b4b6403cf93336a62fe79bf25ab
Anastasiia Tsapiv <nasta.tsv@gmail.com>
2025-12-21 00:09:47 +0200

    mod create

--- create.js
+++ create.js
@@ -8,16 +8,12 @@ document.getElementById("createCarBtn").addEventListener("click", () => {
       price: parseInt(document.getElementById("price").value),
     };
   
-    // Отримуємо існуючі машини з localStorage або створюємо новий масив
     const cars = JSON.parse(localStorage.getItem("cars") || "[]");
   
-    // Додаємо нову машину на початок
     cars.unshift(newCar);
   
-    // Зберігаємо назад у localStorage
     localStorage.setItem("cars", JSON.stringify(cars));
   
-    // Переходимо назад на сторінку зі всіма машинами
     window.location.href = "index.html";
   });
 
